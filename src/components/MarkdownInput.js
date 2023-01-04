f1242e1110cdb9f692de0c966f37b23a9334be4c
Agnivesh Chaubey <agniveshvapi@gmail.com>
2023-01-04 23:02:55 +0530

    project's logic is completed but facing one issue i.e., optput is displaying when the component is re-rendered

--- src/components/MarkdownInput.js
+++ src/components/MarkdownInput.js
@@ -1,16 +1,34 @@
-import React from 'react'
+import React, { createContext, useState } from 'react';
 import { Badge } from "react-bootstrap";
+import { marked } from 'marked';
+import { DataSetter } from './MarkdownPreviewer';
 
+
+export const UserContext = createContext();
 
 const MarkdownInput = () => {
+    const [input, setInput] = useState({ markdown: '' });
+
+
     var inputStyle = {
-        width: "400px",
+        width: "100%",
         height: "50vh",
         marginLeft: "auto",
         marginRight: "auto",
         padding: "10px"
     };
+
+    const handleChange = (markdown) => {
+        setInput({ markdown });
+    }
+
+
+    const convertedData = marked(input.markdown);
+    // console.log(input)
+    // console.log(convertedData)
+
     return (
+        // <UserContext.Provider value={convertedData}>
         <>
             <div className="col text-center">
                 <h4>
@@ -19,10 +37,21 @@ const MarkdownInput = () => {
                     </Badge>
                 </h4>
             </div>
-            <div className="mark-input" style={inputStyle}>
-                <textarea className="input" style={inputStyle}></textarea>
+            <div className="mark-input" >
+                <textarea
+                    value={input.markdown}
+                    className="input"
+                    style={inputStyle}
+                    onChange={e => {
+                        handleChange(e.target.value)
+                    }}
+                ></textarea>
             </div>
+            <DataSetter convertedData={convertedData}/>
         </>
+            // {/* <MarkdownPreviewer /> */ }
+             // {/* <div dangerouslySetInnerHTML={{__html:convertedData}}></div> */ }
+             // {/* // </UserContext.Provider> */}
     )
 }
 
--- src/components/MarkdownPreviewer.js
+++ src/components/MarkdownPreviewer.js
@@ -1,17 +1,40 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import { Badge } from 'react-bootstrap';
+import { UserContext } from './MarkdownInput';
+
+
+let myData = '';
+export const DataSetter = (props) => {
+    myData = props.convertedData;
+    // return convertedData
+}
 
 const MarkdownPreviewer = () => {
+    console.log(myData)
+    var outputStyle = {
+        width: '100%',
+        height: "50vh",
+        backgroundColor: "#DCDCDC",
+        marginLeft: "auto",
+        marginRight: "auto",
+        padding: "10px"
+    };
+
     return (
-        <div className="col text-center">
-            <h4>
-                <Badge className='' varient='secondary' color='secondary'>
-                    Markdown Preview
-                    
-                </Badge>
-            </h4>
-        </div>
-        
+        <>
+            <div className="col text-center">
+                <h4>
+                    <Badge className='' varient='secondary' color='secondary'>
+                        Markdown Preview
+                    </Badge>
+                </h4>
+            </div>
+            <div
+                style={outputStyle}
+                dangerouslySetInnerHTML={{ __html: myData }}
+            >
+            </div>
+        </>
     )
 }
 
