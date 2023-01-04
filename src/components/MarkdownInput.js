ab758f58aed79d6096e8e703e94067adacba2a46
Agnivesh Chaubey <agniveshvapi@gmail.com>
2023-01-04 17:57:32 +0530

    skeleton is ready

---
+++ src/components/MarkdownInput.js
@@ -0,0 +1,15 @@
+import { Badge } from "react-bootstrap";
+
+import React from 'react'
+
+const MarkdownInput = () => {
+  return (
+    <div className="MarkedownInput">
+    <div className="container">
+        
+    </div>
+    </div>
+  )
+}
+
+export default MarkdownInput;
---
+++ src/components/MarkdownPreviewer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const MarkdownPreviewer = () => {
+  return (
+    <div>
+        <textarea name="editor" id="editor" cols="30" rows="10"></textarea>
+        
+    </div>
+  )
+}
+
+export default MarkdownPreviewer
